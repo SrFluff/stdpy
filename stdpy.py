e22e623de9b6444c120c8f7afd809f0a426ad4ba
SrFluff <fluffins007@outlook.com>
2025-04-27 21:46:01 -0400

    Initial commit

---
+++ stdpy.py
@@ -0,0 +1,21 @@
+import os
+
+def cls():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def log(msg):
+    if os.path.exists("log.txt"):
+        f = open("log.txt","a")
+        f.write("[AUTO]:" + msg + "\n")
+        f.close()
+    else:
+        f = open("log.txt","w")
+        f.close()
+        f = open("log.txt","a")
+        f.write("[AUTO]:" + msg + "\n")
+        f.close()
+
+log("Pluh")
